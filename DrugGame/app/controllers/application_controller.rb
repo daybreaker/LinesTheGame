b70d3a5000055b89039eb0c9941eb5949c65f5a8
len@touringplans.com <len@touringplans.com>
2012-07-08 14:30:44 -0400

    Includes minor screen updates and a start at coding CSS for mobile devices.

--- DrugGame/app/controllers/application_controller.rb
+++ DrugGame/app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
+# Filters added to this controller apply to all controllers
+############################################################
+
+
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  
 end
